d7e78e61f48f8466958707dc794deb24b61d4a78
Robert  Wilson <robby@robbook.lan>
2021-02-16 12:21:13 -0500

    updated script

--- arces_scripts/reduce_arces_imgs.py
+++ arces_scripts/reduce_arces_imgs.py
@@ -3,7 +3,7 @@ from redduck.sea_sea_dee import correct_all_imgs
 
 def run_arces_corrections(data_dir, fnames, bias_fname):
 
-	correct_all_imgs(data_dir, img_fname, bias_fname, dark_fname=None, flat_fname=None, dark_exptime=1e6, sci_exptime=1e6, texp_key='EXPTIME', gain=3.8,pixel_mask=None, fringe_frame=None, clean_cosmicrays=False,)
+	correct_all_imgs(data_dir, img_fname, bias_fname, dark_fname=None, flat_fname=None, dark_exptime=1e6, sci_exptime=1e6, texp_key='EXPTIME', gain=3.8,pixel_mask=None, fringe_frame=None, clean_cosmicrays=False,combine_all=True)
 
 	print('ALL DONE')
 
